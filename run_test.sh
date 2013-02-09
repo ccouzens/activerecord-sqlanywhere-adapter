a00c45391e15dfda24e132cb2740a88686a8e9fb
Chris Couzens <chris@chris-iMac.(none)>
2013-02-09 23:38:04 +0000

    Add scrpt to automate running the tests

---
+++ run_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+source /opt/sqlanywhere12/bin64/sa_config.sh
+source ~/.rvm/scripts/rvm
+
+cd ~/Documents/databases/
+if [ ! -e arunit.db ]
+then
+    dbinit -c arunit
+fi
+if [ ! -e arunit2.db ]
+then
+    dbinit -c arunit2
+fi
+dbsrv12 arunit arunit2 >/dev/null &
+sleep 1
+
+cd ~/Documents/rails/activerecord/
+
+SQLANYWHERE=sqlanywhere rake test_sqlanywhere > ../../activerecord-sqlanywhere-adapter/test_output.txt 2> ../../activerecord-sqlanywhere-adapter/test_error.txt
